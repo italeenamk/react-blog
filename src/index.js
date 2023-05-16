9e5a2d9ada7b6d87aea6cdf429a0aa3ef294b5e3
italeenamk <italeenamk@gmail.com>
2023-05-16 15:49:03 +0500

    topbar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <App/>
   </React.StrictMode>
 );
 
