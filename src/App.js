8f6a0a24a803a78a1655e89187d199a7eb133dfe
italeenamk <italeenamk@gmail.com>
2023-05-16 16:39:18 +0500

    creating header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Topbar from "./topbar/Topbar";
+import Header from "./header/Header";
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
       <>
           <Topbar/>
+          <Home/>
       </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import './home.css';
+import Header from "../../header/Header";
+
+const Home = () => {
+  return(
+      <>
+          <div className="home">
+              <Header/>
+              home
+          </div>
+      </>
+  )
+}
+
+export default Home;
