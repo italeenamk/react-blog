ddeb3f344a1b0413d23773fcc370957cfa026278
italeenamk <italeenamk@gmail.com>
2023-05-17 21:06:57 +0500

    complete login page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 
 function App() {
   return (
       <>
           <Topbar/>
-          <Settings/>
+          <Login/>
       </>
   );
 }
