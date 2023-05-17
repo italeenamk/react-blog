21c2f968c6a66c475b6ed40dce4fc4eb8f57ff66
italeenamk <italeenamk@gmail.com>
2023-05-17 19:27:55 +0500

    continue

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Topbar from "./components/topbar/Topbar";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 function App() {
   return (
       <>
           <Topbar/>
-          <Single/>
+          <Write/>
       </>
   );
 }
--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,4 +1,26 @@
 import './write.css';
 
 
+const Write = () => {
+  return(
+      <>
+          <div className="write">
+              <form className="write-form">
+                  <div className="write-form-group">
+                      <label htmlFor="file-input">
+                          <i className="write-icon fa-solid fa-plus"></i>
+                      </label>
+                      <input type="file" id="file-input" style={{display:"none"}}/>
+                      <input type="text" placeholder="Title" className="write-input" autoFocus={true}/>
+                  </div>
+                  <div className="write-form-group">
+                      <textarea className="write-input write-text" placeholder="Tell your story..." type="text"/>
+                  </div>
+                  <button className="write-submit">Publish</button>
+              </form>
+          </div>
+      </>
+  )
+}
 
+export default Write;
