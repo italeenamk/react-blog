56af36872bf498f6e91e36f57168819835aaff29
italeenamk <italeenamk@gmail.com>
2023-05-17 21:05:17 +0500

    complete settings section

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,11 @@
+import './';
+
+
+const Login = () => {
+  return(
+      <>
+      </>
+  )
+}
+
+export default Login;
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -6,14 +6,40 @@ const Settings = () => {
   return(
       <>
           <div className="settings">
-              <div className="settings-wrapper">
-                  <div className="settings-title">
-                      <span className="settings-update-title">Update Your Account</span>
-                      <span className="settings-delete-title">Delete Account</span>
-                      <form className="settings-form"></form>
+              <div className="settingsWrapper">
+                  <div className="settingsTitle">
+                      <span className="settingsTitleUpdate">Update Your Account</span>
+                      <span className="settingsTitleDelete">Delete Account</span>
                   </div>
+                  <form className="settingsForm">
+                      <label>Profile Picture</label>
+                      <div className="settingsPP">
+                          <img
+                              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                              alt=""
+                          />
+                          <label htmlFor="fileInput">
+                              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+                          </label>
+                          <input
+                              id="fileInput"
+                              type="file"
+                              style={{ display: "none" }}
+                              className="settingsPPInput"
+                          />
+                      </div>
+                      <label>Username</label>
+                      <input type="text" placeholder="Safak" name="name" />
+                      <label>Email</label>
+                      <input type="email" placeholder="safak@gmail.com" name="email" />
+                      <label>Password</label>
+                      <input type="password" placeholder="Password" name="password" />
+                      <button className="settingsSubmitButton" type="submit">
+                          Update
+                      </button>
+                  </form>
               </div>
-              <Sidebar/>
+              <Sidebar />
           </div>
       </>
   )
