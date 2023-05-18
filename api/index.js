dcbb53679b8c004107fcb89e0161c8fad10e6500
italeenamk <italeenamk@gmail.com>
2023-05-18 23:08:28 +0500

    api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-console.log("hello")
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONG0_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+    .then(console.log("connected"))
+    .catch((err)=> console.log(err));
+
 
 app.listen("5001", ()=>{
     console.log("backend")
