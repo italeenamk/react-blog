7edf564794222f983b719dcfc8efc17505acd6ed
italeenamk <italeenamk@gmail.com>
2023-05-23 23:03:27 +0500

    login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -24,4 +24,23 @@ router.post("/register", async (req, res)=>{
 
 // login
 
+router.post("/login", async (req, res)=>{
+    try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass,
+        });
+
+        const user = await newUser.save();
+        console.log(user);
+        res.status(200).json(user);
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
