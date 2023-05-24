ea8d4fdad2989853bec76b56bb6f687ba2b5a3eb
italeenamk <italeenamk@gmail.com>
2023-05-24 19:10:54 +0500

    user update

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,10 +3,14 @@ const User = require("../models/User");
 
 // update
 router.post("/:id", async (req, res)=>{
-    try{
-    } catch (err){
-        console.log(err);
-        res.status(500).json(err);
+    if(req.body.userId === req.params.id){
+        try{
+        } catch (err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    } else{
+        res.status(401).json("You can update only your account");
     }
 });
 
