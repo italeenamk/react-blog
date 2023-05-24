c9bc61150507a73e38bd04efeb0f1a237c8fa1d7
italeenamk <italeenamk@gmail.com>
2023-05-24 21:56:32 +0500

    user delete method

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,7 +12,9 @@ router.put("/:id", async (req, res)=>{
         try{
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
-            });
+            },
+                { new:true }
+            );
             res.status(200).json(updatedUser);
         } catch (err){
             console.log(err);
@@ -24,5 +26,26 @@ router.put("/:id", async (req, res)=>{
 });
 
 // delete
+router.delete("/:id", async (req, res)=>{
+    if(req.body.userId === req.params.id){
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
+        try{
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                    $set: req.body,
+                },
+                { new:true }
+            );
+            res.status(200).json(updatedUser);
+        } catch (err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    } else{
+        res.status(401).json("You can update only your account!");
+    }
+});
 
 module.exports = router;
