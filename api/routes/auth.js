2ae208378b9dbee6f0f01e090fd505073699d8be
italeenamk <italeenamk@gmail.com>
2023-05-24 15:53:56 +0500

    login user

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -26,19 +26,9 @@ router.post("/register", async (req, res)=>{
 
 router.post("/login", async (req, res)=>{
     try{
-        const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(req.body.password, salt);
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPass,
-        });
-
-        const user = await newUser.save();
-        console.log(user);
-        res.status(200).json(user);
+        const user = User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong credentials!")
     } catch (err){
-        console.log(err);
         res.status(500).json(err);
     }
 });
