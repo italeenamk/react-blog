03d8adaba8134ce09e853e68e116fa8df19b0eb6
italeenamk <italeenamk@gmail.com>
2023-05-24 21:57:43 +0500

    continue

--- api/routes/users.js
+++ api/routes/users.js
@@ -44,7 +44,7 @@ router.delete("/:id", async (req, res)=>{
             res.status(500).json(err);
         }
     } else{
-        res.status(401).json("You can update only your account!");
+        res.status(401).json("You can delete only your account!");
     }
 });
 
