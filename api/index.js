24affad98689d8e908ad07b87259a33b5457abb6
italeenamk <italeenamk@gmail.com>
2023-05-24 19:51:31 +0500

    user update password

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 
 dotenv.config();
@@ -19,6 +20,7 @@ mongoose
     .catch((err)=> console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5001", ()=>{
     console.log("backend")
