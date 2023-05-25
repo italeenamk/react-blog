8ad498d041306cddb963a83ebf442d096f56a0c9
italeenamk <italeenamk@gmail.com>
2023-05-25 16:12:11 +0500

    delete method

--- api/routes/users.js
+++ api/routes/users.js
@@ -28,17 +28,9 @@ router.put("/:id", async (req, res)=>{
 // delete
 router.delete("/:id", async (req, res)=>{
     if(req.body.userId === req.params.id){
-        if (req.body.password) {
-            const salt = await bcrypt.genSalt(10);
-            req.body.password = await bcrypt.hash(req.body.password, salt);
-        }
         try{
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                    $set: req.body,
-                },
-                { new:true }
-            );
-            res.status(200).json(updatedUser);
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted...");
         } catch (err){
             console.log(err);
             res.status(500).json(err);
