8d1c08287442d752b120e38bedd322bdb60bb291
italeenamk <italeenamk@gmail.com>
2023-05-25 20:58:42 +0500

    continue

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -17,13 +17,54 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
     const  newPost = new Post(req.body);
     try{
-        const post = Post.findById(req.params.id);
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username){
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(
+                    req.params.id,
+                    {
+                        $set: req.body,
+                    },
+                    { new: true }
+                );
+                res.status(200).json(updatedPost);
+            }catch (err){
+                res.status(500).json(err);
+            }
+        } else{
+            res.status(401).json("You can update only your post!");
+        }
     } catch (err){
         res.status(500).json(err);
     }
 });
 
 // delete post
+
+router.delete("/:id", async (req, res) => {
+    const  newPost = new Post(req.body);
+    try{
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username){
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(
+                    req.params.id,
+                    {
+                        $set: req.body,
+                    },
+                    { new: true }
+                );
+                res.status(200).json(updatedPost);
+            }catch (err){
+                res.status(500).json(err);
+            }
+        } else{
+            res.status(401).json("You can delete only your post!");
+        }
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
 
 // get post
 
