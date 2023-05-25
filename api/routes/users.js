904174c89428fdc442e02b32f6705ca2db9211f5
italeenamk <italeenamk@gmail.com>
2023-05-25 16:43:50 +0500

    delete method complete

--- api/routes/users.js
+++ api/routes/users.js
@@ -32,6 +32,7 @@ router.delete("/:id", async (req, res)=>{
         try {
             const user = await User.findById(req.params.id);
             try{
+                await Post.deleteMany({ username: user.username });
                 await User.findByIdAndDelete(req.params.id);
                 res.status(200).json("User has been deleted...");
             } catch (err){
