9fccfbe36cf0116a620c9970996a79e7622a29f3
italeenamk <italeenamk@gmail.com>
2023-06-17 22:14:10 +0500

    data post

--- client/src/components/singlepost/Singlepost.jsx
+++ client/src/components/singlepost/Singlepost.jsx
@@ -11,7 +11,7 @@ const Singlepost = () => {
     useEffect(()=>{
         const getPost = async ()=>{
             const res = await axios.get("http://localhost:5001/api/posts/" + path);
-            console.log(res)
+            setPost(res.data)
         };
         getPost()
     }, [path])
@@ -19,15 +19,17 @@ const Singlepost = () => {
       <>
           <div className="single-post">
               <div className="single-post-wrapper">
-                  <img src="https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="" className="single-post-img"/>
-                  <h1 className="single-post-title">Lorem ipsum dolor sit amet.
+                  {post.photo && (
+                      <img src={post.photo} alt="" className="single-post-img"/>
+                  )}
+                  <h1 className="single-post-title">{post.title}
                       <div className="single-post-edit">
                           <i className="single-post-icon fa-solid fa-pen-to-square"></i>
                           <i className="single-post-icon fa-solid fa-trash"></i>
                       </div>
                   </h1>
                   <div className="single-post-info">
-                      <span className="single-post-author">Author: <b> Aleena</b></span>
+                      <span className="single-post-author">Author: <b> {post.username}</b></span>
                       <span className="single-post-date">1 hour ago</span>
                   </div>
                   <p className="single-post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto
