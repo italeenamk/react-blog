444f121c262a9a972993ad673b3489a10e58c268
italeenamk <italeenamk@gmail.com>
2023-06-18 18:03:01 +0500

    only user get all data

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,9 +4,13 @@ import Posts from "../../components/posts/posts";
 import Sidebar from "../../components/sidebar/sidebar";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {useLocation} from "react-router-dom";
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
+    const location = useLocation()
+
+    console.log(location);
 
     const fetchPosts = async () => {
         const res = await axios.get("http://localhost:5001/api/posts", {
