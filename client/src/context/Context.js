1799242dd1a4d9f76979bcfd7c75dbb16c141deb
italeenamk <italeenamk@gmail.com>
2023-06-20 22:32:15 +0500

    ContextProvider {(children)} function

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,4 +1,5 @@
 import {createContext, useReducer} from "react";
+import Reducer from "./Reducer";
 
 const INITIAL_STATE = {
     user: null,
@@ -8,4 +9,6 @@ const INITIAL_STATE = {
 
 export const Context = createContext(INITIAL_STATE);
 
-export const ContextProvider = ({children}) => {}
+export const ContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(Reducer, INITIAL_STATE)
+}
--- client/src/context/Reducer.js
+++ client/src/context/Reducer.js
@@ -22,3 +22,5 @@ const Reducer = (state, action)=>{
             return state;
     }
 }
+
+export default Reducer;
