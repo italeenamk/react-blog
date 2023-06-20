93d2e0324da571a49c20e00c06a32bfe06c94f8c
italeenamk <italeenamk@gmail.com>
2023-06-20 22:48:03 +0500

    continue

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -8,12 +8,12 @@ const Login = () => {
       <>
           <div className="login">
               <span className="loginTitle">Login</span>
-              <form className="loginForm">
-                  <label>Email</label>
-                  <input className="loginInput" type="text" placeholder="Enter your email..." />
+              <form className="loginForm" onSubmit={handelsubmit}>
+                  <label>Username</label>
+                  <input className="loginInput" type="text" placeholder="Enter your username..." />
                   <label>Password</label>
                   <input className="loginInput" type="password" placeholder="Enter your password..." />
-                  <button className="loginButton">Login</button>
+                  <button className="loginButton" type="submit">Login</button>
               </form>
               <button className="loginRegisterButton"><Link style={{textDecoration: "none", color:"inherit"}} to="/register">Register</Link></button>
           </div>
