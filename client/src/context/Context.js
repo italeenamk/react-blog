aff6e79d320495f0f0ecd1527bc1e1e18453c281
italeenamk <italeenamk@gmail.com>
2023-06-20 22:43:37 +0500

    ContextProvider add in index.js

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -10,5 +10,14 @@ const INITIAL_STATE = {
 export const Context = createContext(INITIAL_STATE);
 
 export const ContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(Reducer, INITIAL_STATE)
+    const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+    return <Context.Provider value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+    }}>
+        {children}
+    </Context.Provider>
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import {ContextProvider} from "./context/Context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+      <ContextProvider>
+          <App/>
+      </ContextProvider>
   </React.StrictMode>
 );
 
