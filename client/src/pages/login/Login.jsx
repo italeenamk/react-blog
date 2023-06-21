b320a256981316977b9a430fa1844e536291853c
italeenamk <italeenamk@gmail.com>
2023-06-21 20:29:30 +0500

    handelsubmit define

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,7 +3,9 @@ import {Link} from "react-router-dom";
 
 
 const Login = () => {
-
+    const handelsubmit=(e)=>{
+      e.preventDefault();
+    };
   return(
       <>
           <div className="login">
