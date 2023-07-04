180746912722d20d6d1406ebf96eef138685af14
italeenamk <italeenamk@gmail.com>
2023-07-04 20:00:41 +0500

    user null condition

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -2,7 +2,7 @@ import {createContext, useEffect, useReducer} from "react";
 import Reducer from "./Reducer";
 
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem("user")),
+    user: JSON.parse(localStorage.getItem("user")) || null,
     isFetching: false,
     error: false,
 };
