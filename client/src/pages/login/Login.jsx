15da142684aaf66e52a8848fe8572c374bbef2cd
italeenamk <italeenamk@gmail.com>
2023-07-05 19:05:32 +0500

    continue

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
                   <input className="loginInput" type="text" placeholder="Enter your username..." ref={userRef}/>
                   <label>Password</label>
                   <input className="loginInput" type="password" placeholder="Enter your password..." ref={passwordRef}/>
-                  <button className="loginButton" type="submit" disabled={ isFetching }>Login</button>
+                  <button className="loginButton" type="submit">Login</button>
               </form>
               <button className="loginRegisterButton"><Link style={{textDecoration: "none", color:"inherit"}} to="/register">Register</Link></button>
           </div>
